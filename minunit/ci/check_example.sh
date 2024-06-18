1da3f850ff767366db416b4d9fd499db66a1f7bb
ricol03 <ricolcp03@gmail.com>
2024-06-18 13:46:09 +0100

    Deletion of unnecessary files

--- minunit/ci/check_example.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-expected_exit_code=6
-./minunit_example
-if [ $? -ne $expected_exit_code ]; then
-  echo "Unexpected exit code: $expected_exit_code expected but was $?"
-  exit 1
-fi
-exit 0
